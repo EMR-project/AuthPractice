bddeb824e7c9e6c010e2cb4c3b6c9a4fe473931d
sanghoonchae <sh27.chae@gmail.com>
2023-04-17 23:36:02 +0900

    NoArgsConstructor error commit

--- src/main/java/com/emr/auth/domain/UserJoinRequest.java
+++ src/main/java/com/emr/auth/domain/UserJoinRequest.java
@@ -1,7 +1,11 @@
 package com.emr.auth.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class UserJoinRequest {
     private String userName;
